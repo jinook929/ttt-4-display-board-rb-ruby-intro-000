e3d37d03b32cd52b51f02b3647cb9c53241656bf
JINOOK JUNG <jinook929@gmail.com>
2020-11-10 02:46:19 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board()
+def display_board(board)
+   puts " #{board[0]} |"
+   puts "-----------"
    
+   puts "-----------"
 end
 
 board = [" "," "," "," "," "," "," "," "," "]
+display_board(board)
